ef218a6144b95eaea1cfa1346f497ff292d56008
Ilya Kharlamov <ilia3546@me.com>
2023-02-07 17:03:21 +0300

    Tuist compatible

---
+++ Project.swift
@@ -0,0 +1,21 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project(
+    name: .GeoHelper,
+    targets: [
+        Target(
+            name: .GeoHelper,
+            dependencies: [
+                .external(name: .Alamofire)
+            ]
+        ),
+        Target(
+            name: .GeoHelperTests,
+            product: .unitTests,
+            sources: .defaultTestsPath,
+            dependencies: [.target(name: .GeoHelper)]
+        )
+    ],
+    additionalFiles: ["README.MD", "Package.swift", "GeoHelper.podspec"]
+)
